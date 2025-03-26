1fe695617b324b28f3c08994a1b4affe474400a3
Niklas <niklas@nikkl.me>
2025-03-26 21:51:22 +0100

    chore: remove environment configuration files and add bruno collection
    Signed-off-by: Niklas <niklas@nikkl.me>

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
+  app.setGlobalPrefix(configService.get('APP_PREFIX') || 'api');
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
