9254bfa5b747e0d8f4a266240a95cd0f442cfbd7
Niklas <niklas@nikkl.me>
2025-03-27 22:49:49 +0100

    feat(throttling): integrate throttler module with custom rate limits for enhanced request handling
    Signed-off-by: Niklas <niklas@nikkl.me>

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,12 +5,37 @@ import { GlobalModule } from './common/global/global.module';
 import { AuthModule } from './core/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard, ResourceGuard, RoleGuard } from 'nest-keycloak-connect';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [GlobalModule, AuthModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 5,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 40,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+    GlobalModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
