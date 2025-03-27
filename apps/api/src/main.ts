c116142e43d58e3b391004b51e5ba4733045b91d
Niklas <niklas@nikkl.me>
2025-03-27 22:44:11 +0100

    feat(security): add helmet middleware for enhanced security
    Signed-off-by: Niklas <niklas@nikkl.me>

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { I18nService } from 'nestjs-i18n';
+import helmet from 'helmet';
 
 export let i18nService: I18nService;
 
@@ -12,6 +13,7 @@ async function bootstrap() {
 
   i18nService = app.get(I18nService);
 
+  app.use(helmet()); // must be first middleware, see https://docs.nestjs.com/security/helmet
   app.setGlobalPrefix(configService.get('APP_PREFIX') || 'api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
